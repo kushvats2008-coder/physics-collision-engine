7a12ee3c48f785fb213449b806101812f3583919
Kush Vats <kush.vats2008@gmail.com>
2026-09-06 13:30:21 +0530

    Stop tracking generated build and dependency files

--- build/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/projects/physicsobject/build/_deps/flac-src/src/libFLAC
-# Build directory: E:/projects/physicsobject/build/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
